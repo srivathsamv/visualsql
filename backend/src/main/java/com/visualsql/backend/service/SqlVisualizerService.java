9579c8a85af298860aeac3d3fa92c732a9e85d3b
Srivathsa Mantrala <srivathsamv2@gmail.com>
2026-07-04 19:46:10 -0400

    Added service method

--- backend/src/main/java/com/visualsql/backend/service/SqlVisualizerService.java
+++ backend/src/main/java/com/visualsql/backend/service/SqlVisualizerService.java
@@ -1,4 +1,8 @@
 package com.visualsql.backend.service;
 
+import com.visualsql.backend.dto.GraphResponse;
+
 public interface SqlVisualizerService {
+
+    GraphResponse visualize(String sql);
 }
--- backend/src/main/java/com/visualsql/backend/service/impl/SqlVisualizerServiceImpl.java
+++ backend/src/main/java/com/visualsql/backend/service/impl/SqlVisualizerServiceImpl.java
@@ -1,6 +1,12 @@
 package com.visualsql.backend.service.impl;
 
+import com.visualsql.backend.dto.GraphResponse;
 import com.visualsql.backend.service.SqlVisualizerService;
 
 public class SqlVisualizerServiceImpl implements SqlVisualizerService {
+
+    @Override
+    public GraphResponse visualize(String sql) {
+        return null;
+    }
 }
